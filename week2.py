c0b701bd5981e411934b4c09df1b7ed9e8fe6e15
Federico Baroni <fbaroni@santafe.gov.ar>
2017-03-22 11:35:04 -0300

    week 2

---
+++ week2.py
@@ -0,0 +1,41 @@
+#For the purchase records from the pet store, how would you get a list of all items
+#which had been purchased (regardless of where they might have been
+#purchased, or by whom)?
+
+purchase_1 = pd.Series({'Name': 'Chris',
+                        'Item Purchased': 'Dog Food',
+                        'Cost': 22.50})
+purchase_2 = pd.Series({'Name': 'Kevyn',
+                        'Item Purchased': 'Kitty Litter',
+                        'Cost': 2.50})
+purchase_3 = pd.Series({'Name': 'Vinod',
+                        'Item Purchased': 'Bird Seed',
+                        'Cost': 5.00})
+
+df = pd.DataFrame(
+    [purchase_1, purchase_2, purchase_3],
+    index=['Store 1', 'Store 1', 'Store 2']
+)
+# Your code here
+df['Item Purchased']
+
+
+#For the purchase records from the pet store, how would you update the DataFrame,
+#applying a discount of 20% across all the values in the 'Cost' column?
+
+purchase_1 = pd.Series({'Name': 'Chris',
+                        'Item Purchased': 'Dog Food',
+                        'Cost': 22.50})
+purchase_2 = pd.Series({'Name': 'Kevyn',
+                        'Item Purchased': 'Kitty Litter',
+                        'Cost': 2.50})
+purchase_3 = pd.Series({'Name': 'Vinod',
+                        'Item Purchased': 'Bird Seed',
+                        'Cost': 5.00})
+
+df = pd.DataFrame([purchase_1, purchase_2, purchase_3], index=['Store 1', 'Store 1', 'Store 2'])
+
+
+# Your answer here
+df['Cost'] *= 0.8
+df
